a38818f109962b66cf2e448b114ef64492386316
Sachin Lagad <sachinlagad56@gmail.com>
2022-10-21 18:28:55 +0530

    Employee Wage Computaion initial commit

---
+++ com/bridgelabz/day3/EmployeeWage.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.day3;
+
+public class EmployeeWage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome to employee wage computaion program!");
+	}
+
+}
