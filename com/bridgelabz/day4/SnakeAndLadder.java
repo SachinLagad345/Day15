038057421068d204ed356ea6c998d1b9758561be
Sachin Lagad <sachinlagad56@gmail.com>
2022-10-22 19:43:59 +0530

    Initial commit for Snake and Ladder

---
+++ com/bridgelabz/day4/SnakeAndLadder.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.day4;
+
+public class SnakeAndLadder {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome to game");
+		
+	}
+
+}
