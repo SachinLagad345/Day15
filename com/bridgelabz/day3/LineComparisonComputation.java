2c72f37bea7bc362e4c498cbd910e927bdc814e7
Sachin Lagad <sachinlagad56@gmail.com>
2022-10-22 17:01:03 +0530

    Added UC2 check equality using equals method

--- com/bridgelabz/day3/LineComparisonComputation.java
+++ com/bridgelabz/day3/LineComparisonComputation.java
@@ -19,10 +19,24 @@ public class LineComparisonComputation {
 		int x2 = sc.nextInt();
 		System.out.println("Enter end y co-ordinate for first line");
 		int y2 = sc.nextInt();
-		double l1 = Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));
+		Double l1 = new Double( Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2)));
 		
 		System.out.println("Length of line is "+ l1);
 		
+		System.out.println("Enter starting x co-ordinate for second line");
+		int x3 = sc.nextInt();
+		System.out.println("Enter starting y co-ordinate for second line");
+		int y3 = sc.nextInt();
+		System.out.println("Enter end x co-ordinate for second line");
+		int x4 = sc.nextInt();
+		System.out.println("Enter end y co-ordinate for second line");
+		int y4 = sc.nextInt();
+		Double l2 = new Double(Math.sqrt(Math.pow(x4-x3,2)+Math.pow(y4-y3,2)));
+		
+		if(l1.equals(l2))
+			System.out.println("L1 and L2 are equal");
+		else
+			System.out.println("L1 and L2 are not equal");
 		
 	}
 
