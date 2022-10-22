783fbe00eda9410479056983902004d671e81080
Sachin Lagad <sachinlagad56@gmail.com>
2022-10-22 16:34:05 +0530

    Initial commit for Line Computation

---
+++ com/bridgelabz/day3/LineComparisonComputation.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.day3;
+
+public class LineComparisonComputation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Welcome to Line Comparison Computation Program!");
+	}
+
+}
