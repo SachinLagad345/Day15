c53c771f32d23737513bc8aa2d28b4cd462544b2
Sachin Lagad <sachinlagad56@gmail.com>
2022-10-26 16:54:16 +0530

    Added corrections

--- com/bridgelabz/day5/Triplets.java
+++ com/bridgelabz/day5/Triplets.java
@@ -18,24 +18,47 @@ public class Triplets {
         	arr[i] = num;
         }
         
+        int unqar[] = new int[arr.length];
+        int unqhead = 0;
+        int arr2[] = arr.clone();
+        
+        for(int i=0;i<arr.length;i++)
+        {
+        	int counter = 0;
+        	for(int j=i;j<arr.length;j++)
+        	{
+        		if(arr[i] == arr2[j] && counter==0)
+        			counter++;
+        		else if(arr[i] == arr2[j])
+        		{
+        			arr2[j] = arr2[j] + 1;
+        		}
+        	}
+        }
+        
+        for(int i=0;i<arr.length;i++)
+        {
+        	if(arr[i] == arr2[i])
+        	{
+        		unqar[unqhead] = arr[i];
+        		unqhead++;
+        	}
+        }
         int tripletsArr[] = new int[3*arr.length];
         int head = 0;
-        for(int i=0;i<arr.length-2;i++)
+        for(int i=0;i<unqhead-2;i++)
         {
-        	for(int j=i+1;j<arr.length-1;j++)
+        	for(int j=i+1;j<unqhead-1;j++)
         	{
-        		if(arr[i]==arr[j])
-        			continue;
-        		for(int k=j+1;k<arr.length;k++)
+        		for(int k=j+1;k<unqhead;k++)
         		{
         			System.out.println(" i = " + i + " j = "+j+" k = "+k);
-        			if(arr[j]==arr[k])
-        				continue;
-        			if((arr[i]+arr[j]+arr[k]) == 0)
+        			
+        			if((unqar[i]+unqar[j]+unqar[k]) == 0)
         			{
-        				tripletsArr[head++] = arr[i];
-        				tripletsArr[head++] = arr[j];
-        				tripletsArr[head++] = arr[k];
+        				tripletsArr[head++] = unqar[i];
+        				tripletsArr[head++] = unqar[j];
+        				tripletsArr[head++] = unqar[k];
         			}
         		}
         	}
