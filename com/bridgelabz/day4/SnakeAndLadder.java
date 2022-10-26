49568cd3d17befc5b46d6b1a4334e015a3676df4
Sachin Lagad <sachinlagad56@gmail.com>
2022-10-26 13:33:12 +0530

    Added UC3 check no play,ladder,snake

--- com/bridgelabz/day4/SnakeAndLadder.java
+++ com/bridgelabz/day4/SnakeAndLadder.java
@@ -16,6 +16,26 @@ public class SnakeAndLadder {
 		
 		int n = 1 + rn.nextInt(6);
 		System.out.println("Player 1 has got " + n);
+		
+		int opt = rn.nextInt(3);
+		
+		if(opt==0)
+		{
+			System.out.println("No play");
+		}
+		else if(opt == 1)
+		{
+			System.out.println("Ladder!!");
+			sn.pos += n;
+		}
+		else
+		{
+			System.out.println("Snake!!");
+			if(sn.pos >= n)
+				sn.pos -= n;
+			else
+				sn.pos = 0;
+		}
 	}
 
 }
