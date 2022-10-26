476d37965c6a3ed1f6f1105e137fc2f9ea3680d7
Sachin Lagad <sachinlagad56@gmail.com>
2022-10-26 13:41:03 +0530

    Added UC4 repeat till 100

--- com/bridgelabz/day4/SnakeAndLadder.java
+++ com/bridgelabz/day4/SnakeAndLadder.java
@@ -14,27 +14,36 @@ public class SnakeAndLadder {
 		
 		Random rn = new Random();
 		
-		int n = 1 + rn.nextInt(6);
-		System.out.println("Player 1 has got " + n);
-		
-		int opt = rn.nextInt(3);
-		
-		if(opt==0)
+		while(sn.pos < 100)
 		{
-			System.out.println("No play");
-		}
-		else if(opt == 1)
-		{
-			System.out.println("Ladder!!");
-			sn.pos += n;
-		}
-		else
-		{
-			System.out.println("Snake!!");
-			if(sn.pos >= n)
-				sn.pos -= n;
+			int n = 1 + rn.nextInt(6);
+			System.out.println("Player 1 has got " + n);
+			
+			int opt = rn.nextInt(3);
+			
+			if(opt==0)
+			{
+				System.out.println("No play");
+			}
+			else if(opt == 1)
+			{
+				sn.pos += n;
+				System.out.println("Ladder!! New position is " + sn.pos);
+			}
 			else
-				sn.pos = 0;
+			{
+				if(sn.pos >= n)
+				{
+					sn.pos -= n;
+					System.out.println("Snake!! New position is " + sn.pos);
+				}
+				else
+				{
+					sn.pos = 0;
+					System.out.println("Snake!! New position is " + sn.pos);
+				}
+			}
+		
 		}
 	}
 
