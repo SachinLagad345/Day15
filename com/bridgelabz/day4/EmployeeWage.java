16d55084ddea37a8721bf518a6bcebce54cb5de6
Sachin Lagad <sachinlagad56@gmail.com>
2022-10-26 15:58:48 +0530

    Added UC4 switch case in employee

--- com/bridgelabz/day4/EmployeeWage.java
+++ com/bridgelabz/day4/EmployeeWage.java
@@ -33,22 +33,36 @@ public class EmployeeWage {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			
-			System.out.println("Welcome to employee wage computaion program!");
-			Random rnd = new Random();
-			int p = rnd.nextInt(3);
-			EmployeeWage emp = new EmployeeWage();
-			if(emp.isEmployeePresent(p))
-				System.out.println("Employee is present!");
-			else
-				System.out.println("Employee is absent!");
-			
-			if(p == 2)
-				System.out.println("Employee is part time");
-			else
-				System.out.println("Employee is full time");
-			
-			int wage = emp.calculateWage(p);
-			System.out.println("Employee wage is " + wage);
+		System.out.println("Welcome to employee wage computaion program!");
+		Random rnd = new Random();
+		int p = rnd.nextInt(3);
+		EmployeeWage emp = new EmployeeWage();
+//		if(emp.isEmployeePresent(p))
+//			System.out.println("Employee is present!");
+//		else
+//			System.out.println("Employee is absent!");
+//		
+//		if(p == 2)
+//			System.out.println("Employee is part time");
+//		else
+//			System.out.println("Employee is full time");
+//		
+//		int wage = emp.calculateWage(p);
+//		System.out.println("Employee wage is " + wage);
+		
+		switch(p)
+		{
+		case 0: System.out.println("Employee is absent");
+		  		System.out.println("Employee wage is " + emp.calculateWage(0));
+		  		break;
+		case 1: System.out.println("Employee is present full time");
+  				System.out.println("Employee wage is " + emp.calculateWage(1));
+  				break;
+		case 2:System.out.println("Employee is present part time");
+  				System.out.println("Employee wage is " + emp.calculateWage(2));
+  				break;
+  		default:break;
+		}
 			
 		}
 
