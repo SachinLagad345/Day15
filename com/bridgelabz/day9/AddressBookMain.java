815c094cc9d7b9df6fd8f705b965c3f46da1c4e6
Sachin Lagad <sachinlagad56@gmail.com>
2022-11-06 21:27:09 +0530

    Initial commit for address book

---
+++ com/bridgelabz/day9/AddressBookMain.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.day9;
+
+public class AddressBookMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome to address book");
+	}
+
+}
