4d0c5e866895d73684a724a89de760ec2ed15343
Sachin Lagad <sachinlagad56@gmail.com>
2022-11-15 16:48:29 +0530

    Added UC2 select letter game

--- com/bridgelabz/day7/TIcTacToeGame.java
+++ com/bridgelabz/day7/TIcTacToeGame.java
@@ -1,8 +1,12 @@
 package com.bridgelabz.day7;
 
+import java.util.Scanner;
+
 public class TIcTacToeGame {
 
 	char board[] = new char[10];
+	char mychar = 'x';
+	char compchar = 'o';
 	
 	public void display()
 	{
@@ -31,10 +35,28 @@ public class TIcTacToeGame {
 		}
 	}
 	
+	
+	public void chooseLetter()
+	{
+		System.out.println("Please choose your Letter!\n Press 1 for X\n Press 2 for O");
+		Scanner sc = new Scanner(System.in);
+		int i = sc.nextInt();
+		if(i==1) {
+			mychar = 'X';
+			compchar = 'O';
+		}
+		else
+		{
+			mychar = 'O';
+			compchar = 'X';
+		}
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		TIcTacToeGame game = new TIcTacToeGame();
+		game.chooseLetter();
 		game.display();
 	}
 
