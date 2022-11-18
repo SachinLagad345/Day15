a0e789456a3fd51e1460f49a65c54cf5e6015d90
Sachin Lagad <sachinlagad56@gmail.com>
2022-11-18 21:40:42 +0530

    Initial commit for linkedlist

---
+++ com/bridgelabz/day14/LinkedList.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.day14;
+
+class Node {
+	String data;
+	Node next;
+	
+	Node(String data)
+	{
+		this.data = data;
+		this.next = null;
+	}
+}
+
+public class LinkedList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		LinkedList li = new LinkedList();
+	}
+
+}
