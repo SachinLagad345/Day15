02ab78801d0421554b0dd6dfc388c9aff4002341
Sachin Lagad <sachinlagad56@gmail.com>
2022-11-21 18:14:05 +0530

    Initial commit for stack

---
+++ com/bridgelabz/day14/StackQueue.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.day14;
+
+class Stack{
+	LinkedList li;
+}
+
+public class StackQueue {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome");
+	}
+
+}
